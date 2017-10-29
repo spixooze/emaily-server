ecf8fcd9f72934d9d88edecf40377997d4fcd8a3
spixooze <s_simon_s@hotmail.com>
2017-10-29 12:05:05 +0100

    auth login, passport.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const { mongoURI, cookieKey } = require('./config/keys')
+require('./models/User')
+require('./services/passport')
+
+mongoose.Promise = global.Promise
+mongoose
+  .connect(mongoURI, { useMongoClient: true })
+  .then(() => console.log('Connected to mLab'))
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`)
+  console.log(`server is running on port ${PORT}`)
 })
