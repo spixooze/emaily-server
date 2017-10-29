34fd3811a40d9940016ca94739c606335426b671
spixooze <s_simon_s@hotmail.com>
2017-10-29 19:06:57 +0100

    prod keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// Keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+  // return prod keys
+  module.exports = require('./prod')
+} else {
+  // return dev keys
+  module.exports = require('./dev')
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,11 @@ module.exports = app => {
 
   app.get('/auth/google/callback', passport.authenticate('google'))
 
+  app.get('/api/logout', (req, res) => {
+    req.logout() // kill the cookie id
+    res.send(req.user)
+  })
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user)
   })
