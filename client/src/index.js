2176812e3ed267fefd2c7b559624ec1fa9280290
spixooze <s_simon_s@hotmail.com>
2018-02-19 12:30:56 +0100

    Mailer. surveyRoutes in progress

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,10 @@ import reduxThunk from 'redux-thunk'
 import App from './components/App'
 import reducers from './reducers'
 
+// axios window for dev
+import axios from 'axios'
+window.axios = axios
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
